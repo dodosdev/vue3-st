ee36773e304e4821bf66318c4a2cbd228e4fce4c
DOJIN <djsong33@gmail.com>
2025-05-04 22:28:08 +0900

    변수 정의

---
+++ movie-info/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>영화 정보</h1>   
+  <p>영화제목 1</p>
+  <p>개봉: 2023</p>
+  <p>장르 : 액션</p>
+</template>
+
+<script>
+//데이터 작성 기능들 정의
+export default {
+  name: 'App',
+  data() {
+    return{
+      
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
