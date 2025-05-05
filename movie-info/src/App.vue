9b848122df4d801f275a2e514243f0bb3e22ba25
DOJIN <djsong33@gmail.com>
2025-05-05 23:03:11 +0900

    data

--- movie-info/src/App.vue
+++ movie-info/src/App.vue
@@ -16,50 +16,86 @@
 
     <!-- 영화 리스트 -->
     <div v-for="(movie, i) in filteredMovies" :key="i" class="movie">
+
       <figure>
+        <img :src="`${movie.imgUrl}`" :alt="movie.title"> 
+        <!-- :src --이미지 경로를 동적으로 바꿔줌 (``)문자와변수를 같이작성가능 -->
       </figure>
-      <h3 class="bg-yellow" :style="movie.textRed">{{ movie.title }}</h3>
-      <p>개봉: {{ movie.year }}</p>
-      <p>장르: {{ movie.category }}</p>
-      <button @click="increaseLike(i)">좋아요</button>
-      <span>{{ movie.like }}</span>
+
+      <!-- 영화 정보 -->
+      <div class="info">
+        <h3 class="bg-yellow" :style="movie.textRed">{{ movie.title }}</h3>
+        <p>개봉: {{ movie.year }}</p>
+        <p>장르: {{ movie.category }}</p>
+        <button 
+          @click="increaseLike(i)">좋아요
+        </button>
+        <span>{{ movie.like }}</span>
+        <!-- 모달 열리는 버튼 -->
+        <p>
+          <button @click="isModal=true">상세보기</button>
+        </p>
+      </div>
+
+      <!-- modal -->
+      <div class="modal" v-if="isModal">
+        <div class="inner">
+          <h3>data[1]</h3>
+          <p>영화 상세보기</p>
+          <button @click="isModal=false">닫기</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
+<!--자바스크립트 변수나 데이터를 HTML에 꽂아넣는 데이터바인딩입니다.
+그걸 {{데이터}} 이런 문법으로 HTML 중간중간에 쉽게 꽂아넣을 수 있습니다. -->
+
+
 <script>
+import data from "./assets/movies"; //data.js에서 데이터 가져오기
+console.log(data);
+
+
 export default {
   name: "App",
   data() {
     return {
+      // isModal: false, //모달창 보이지 않는상태
+      isModal: false, //모달창 보임
       movies: [
         {
           title: "미드",
           year: 2026,
           category: "액션",
-          textRed: "color:red",
+          textRed: "color:black",
           like: 0,
+          imgUrl:'./assets/jp-dorama.png',
         },
         {
           title: "노랑",
           year: 2023,
-          category: "액션, 판타지, 어드벤처",
-          textRed: "color:red",
+          category: "드라마, 액션",
+          textRed: "color:black",
           like: 0,
+          imgUrl:'./assets/mirai.jpeg',
         },
         {
           title: "드라마",
           year: 2024,
-          category: "드라마, 힐링",
-          textRed: "color:red",
+          category: "드라마, 판타지",
+          textRed: "color:black",
           like: 0,
+          imgUrl:'./assets/vini.jpeg',
         },
         {
           title: "일드",
           year: 2025,
           category: "힐링, 판타지",
-          textRed: "color:orange",
+          textRed: "color:black",
           like: 0,
+          imgUrl:'./assets/jp-dorama.png',
         },
       ],
       genreTabs: ["전체", "액션", "드라마", "판타지"], //장르 탭
@@ -83,11 +119,11 @@ export default {
 </script>
 
 <style scoped>
-.bg-yellow {
-  background-color: gold;
+/* .bg-yellow {
   padding: 10px;
-}
+} */
 
+/* tab menu */
 .tabs {
   display: flex;
   gap: 10px;
@@ -103,9 +139,44 @@ export default {
   background: #222;
   color: white;
 }
+
+/* 영화 리스트 */
 .movie {
+  display: flex;
+  justify-content: flex-start;
   margin-bottom: 20px;
   border-bottom: 1px solid #ddd;
   padding-bottom: 10px;
 }
+img{
+  width: 200px;
+  height: 300px;
+}
+
+button{
+  margin-right: 10px;
+  margin-top: 1rem;
+}
+
+
+/* 모달창 */
+.modal{
+  background: rgba(0, 0,0, 0.3);
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal .inner{
+  background: white;
+  width: 80%;
+  padding: 20px;
+  border-radius: 10px;
+}
+
 </style>
---
+++ movie-info/src/assets/movies.js
@@ -0,0 +1,40 @@
+// let food = "mango";
+// let city = "seoul";
+
+// export { food, city }
+
+const data = [
+      {
+        title: "미드",
+        year: 2026,
+        category: "액션",
+        textRed: "color:black",
+        like: 0,
+        imgUrl:'./assets/jp-dorama.png',
+      },
+      {
+        title: "노랑",
+        year: 2023,
+        category: "드라마, 액션",
+        textRed: "color:black",
+        like: 0,
+        imgUrl:'./assets/mirai.jpeg',
+      },
+      {
+        title: "드라마",
+        year: 2024,
+        category: "드라마, 판타지",
+        textRed: "color:black",
+        like: 0,
+        imgUrl:'./assets/vini.jpeg',
+      },
+      {
+        title: "일드",
+        year: 2025,
+        category: "힐링, 판타지",
+        textRed: "color:black",
+        like: 0,
+        imgUrl:'./assets/jp-dorama.png',
+      },
+]
+export default data;  
